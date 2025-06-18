783eb6a3b5aaa441ef84f48319f7d282782781c6
alxn787 <alenabraham787@gmail.com>
2025-06-19 00:30:20 +0530

    init

--- programs/fanch/src/lib.rs
+++ programs/fanch/src/lib.rs
@@ -6,11 +6,49 @@ declare_id!("EDZMdYr4E4LbHJpSBAYXmXCPZYWeGB31fMJ7mp7yTj4t");
 pub mod fanch {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init(ctx:Context<Initialize>, init_value:u32)->Result<()>{
+        ctx.accounts.account.num = init_value;
         Ok(())
     }
+
+    pub fn add(ctx:Context<Add>, num:u32)->Result<()>{
+        ctx.accounts.account.num = ctx.accounts.account.num + num;
+        Ok(())
+    }
+     pub fn double(ctx:Context<Double>)->Result<()>{
+        ctx.accounts.account.num = ctx.accounts.account.num * 2;
+        Ok(())
+     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+struct DataShape{
+    pub num:u32
+}
+
+#[derive(Accounts)] 
+pub struct Initialize<'info>{
+    #[account(init,payer = signer,space = 8+4)]
+    pub account: Account<'info, DataShape>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    signer: Signer<'info>
+}
+
+#[derive(Accounts)] 
+pub struct Double<'info>{
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    signer: Signer<'info>
+}
+
+#[derive(Accounts)] 
+pub struct Add<'info>{
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    signer: Signer<'info>
+}
+
+
